8953de19404acd4185f9f7024e3e9e4b2e616e1b
Sameer Faridi <s.faridi007@gmail.com>
2023-08-16 18:49:28 +0530

    start to make a folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { CssBaseline } from '@mui/material'
-// import {Route, Switch} from 'react-router-dom'
+// import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <CssBaseline>
-        <main>
-          <Router>
-            <Switch path="/">
-              <h1>Home</h1>
-            </Switch>
-          </Router>
-        </main>
+      <CssBaseline>
+        <mian>
+          <Switch>
+            <Route exact path="/movie/:id">
+              <h1>Movie Information</h1>
+            </Route>
+            <Route exact path="/actors/:id">
+              <h1>Acotors</h1>
+            </Route>
+            <Route exact path="/">
+              <h1>Movies</h1>
+            </Route>
+            <Route exact path="/profile/:id">
+              <h1>Profile</h1>
+            </Route>
+          </Switch>
+        </mian>
       </CssBaseline>
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
