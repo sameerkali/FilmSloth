078394c6d3275f742290e03ac11dd76d378138c0
Sameer Faridi <s.faridi007@gmail.com>
2023-08-16 18:03:54 +0530

    install material UI after suffering here and there

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material'
+// import {Route, Switch} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <CssBaseline>
+
+    <h1>App by sameer</h1>
+    </CssBaseline>
+
+    </>
+  )
+}
+
+export default App
