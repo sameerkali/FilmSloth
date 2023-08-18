5dd3136fbe8f0cf51d211c9a44aa7ee1359e76f2
Sameer Faridi <s.faridi007@gmail.com>
2023-08-18 19:43:14 +0530

    adding MUI styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,32 +2,37 @@
 import { CssBaseline } from "@mui/material";
 import { Route, Switch } from "react-router-dom";
 import { Actors, MovieInformation, Movies, NavBar, Profile } from "./index";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
     <>
-      <CssBaseline>
-        <NavBar />
-        <mian>
-          <Switch>
-            <Route exact path="/">
-              <Movies />
-            </Route>
+      <div className={classes.root}>
+        <CssBaseline>
+          <NavBar />
+          <mian className={classes.content}>
+            <div className={classes.toolbar} />
+            <Switch>
+              <Route exact path="/">
+                <Movies />
+              </Route>
 
-            <Route exact path="/movie/:id">
-              <MovieInformation />
-            </Route>
+              <Route exact path="/movie/:id">
+                <MovieInformation />
+              </Route>
 
-            <Route exact path="/actors/:id">
-              <Actors />
-            </Route>
+              <Route exact path="/actors/:id">
+                <Actors />
+              </Route>
 
-            <Route exact path="/profile/:id">
-              <Profile />
-            </Route>
-          </Switch>
-        </mian>
-      </CssBaseline>
+              <Route exact path="/profile/:id">
+                <Profile />
+              </Route>
+            </Switch>
+          </mian>
+        </CssBaseline>
+      </div>
     </>
   );
 };
